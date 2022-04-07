2e919323a9cb0e652416a47484df73c17fb77809
Nazar Shpak <nazar.shpak2004@gmail.com>
2022-04-07 23:09:42 +0300

    Add project files.

---
+++ lesson_5(2)/lesson_5(2).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int sumTo ()
+{
+    int value;
+    int sum=0;
+    std::cout << "Enter number: ";
+    std::cin >> value;
+
+    for (int i=1; i <= value; i++)
+    {
+         sum = sum + i;
+         if (i == value)
+         {
+             std::cout << i;
+         }
+         else
+         {
+             std::cout << i << "+";
+         }
+    }
+    return sum;
+}
+int main()
+{
+    std::cout << "="<<sumTo();
+    return 0;
+}
